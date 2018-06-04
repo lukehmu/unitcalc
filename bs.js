493de1bacbceeda63615f8a94bb896ad6a5497ad
Luke Davies <lukehmu@gmail.com>
2018-06-04 18:50:26 +0100

    basic form inputs

--- bs.js
+++ bs.js
@@ -3,31 +3,77 @@
 
 Vue.component('input-text', {
     data: function () {
-      return {
-        inputValue: '',
-      }
+        return {
+            inputValue: '',
+        }
     },
-    props: ['label', 'newAtt'],
-    template: '<div class="form-group"><label> {{ label }} </label><input v-model="inputValue" class="form-control"></input></div>',
-    created() {
-        this.inputValue = this.newAtt
-    }
-  })
+    props: ['label', 'id'],
+    template: '<div class="form-group"><label :for="id"> {{ label }} </label><input v-model="inputValue" type="text" :id="id" class="form-control"></input></div>',
+})
+
+Vue.component('input-textarea', {
+    data: function () {
+        return {
+            inputValue: '',
+        }
+    },
+    props: ['label', 'id'],
+    template: '<div class="form-group"><label :for="id"> {{ label }} </label><textarea v-model="inputValue" type="text" :id="id" class="form-control"></textarea></div>',
+})
+
+Vue.component('input-select', {
+    data: function () {
+        return {
+            selectedValue: '',
+        }
+    },
+    props: ['label', 'id', 'options'],
+    template: `
+    <div class="form-group">
+        <label :for="id"> {{ label }} </label>
+        <select v-model="selectedValue" :id="id" class="form-control">
+            <option v-for="(option, key, index) in options" :value="key">
+                {{ option }}
+            </option>
+        </select>
+    </div>`
+})
+
+Vue.component('input-radio', {
+    data: function () {
+        return {
+            selectedValue: '',
+        }
+    },
+    props: ['label', 'name', 'options'],
+    template: `
+    <fieldset class="form-group">
+        <legend class="col-form-label">{{ label }}</legend>
+            <div class="form-check" v-for="option in options">
+                <input type="radio" :name="name" :id="option.value" :value="option.value" class="form-check-input" v-model="selectedValue">
+                <label :for="option.value" class="form-check-label"> {{ option.label }} </label>
+            </div>
+        </legend>
+    </fieldset>`
+})
+{/* <input type="radio" id="contactChoice1"
+name="contact" value="email">
+<label for="contactChoice1">Email</label> */}
 
 
 
 var app = new Vue({
     el: '#app',
-    
+
     data: {
         questions: '',
         options: ''
     },
     computed: {
-        
+
     },
     methods: {
-        
+
     },
     mounted() {
         console.log('App mounted')
@@ -39,17 +85,17 @@ var app = new Vue({
         }
     },
     watch: {
-        
+
     },
     created() {
         axios({
-            method: 'GET',
-            url: 'https://s3-eu-west-1.amazonaws.com/files.epwebsites.co.uk/yaml.json'
-          })
-          .then (response => {
-              this.questions = response.data.inputs
-              this.options = response.data.options
-          })
+                method: 'GET',
+                url: 'https://s3-eu-west-1.amazonaws.com/files.epwebsites.co.uk/yaml.json'
+            })
+            .then(response => {
+                this.questions = response.data.inputs
+                this.options = response.data.options
+            })
     }
 
 })
